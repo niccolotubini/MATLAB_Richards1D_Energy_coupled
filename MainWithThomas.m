41f3604446e0a89c397321e0f8df9fbba00cab59
niccolotubini <tubini.niccolo@gmail.com>
2018-10-09 17:22:08 +0200

    Modified to couple surface and subsurface flow

--- MainWithThomas.m
+++ MainWithThomas.m
@@ -1,7 +1,9 @@
-% BTCS scheme for the 1D Richards equation using the nested Newton method of
-% Casulli & Zanolli.
-% Ricahrds equation is coupled with the thermal problem
-% THOMAS Algorithm
+%% BTCS scheme for the 1D Richards equation using the nested Newton method of
+% Casulli & Zanolli 2005, 2010, 2017.
+% This code integrate the mass conservation equation (Richards' equation
+% coupled with surface flow) and the energy conservation equation written
+% in conservative form.
+% Resulting system are solved with Thomas algorithm
 
 clear all
 close all
@@ -9,6 +11,7 @@ clc
 global alpha thetas thetar n m Ks psic cW rho K KL KR TR TL di IMAX dx dt
 
 %Phisical model parameters in SI units
+thetaMethod = 1;
 day     = 24*3600;
 Ks      = 0.062/day;    %[meter/second]
 thetas  = 0.41;         %[-] saturated water content
@@ -26,27 +29,33 @@ xR = 2;                 %surface
 IMAX = 320;             %number of control volumes
 dx = (xR-xL)/IMAX;      %mesh spacing
 x = linspace(xL+dx/2,xR-dx/2,IMAX);
+x = [x,xR];
 xv = linspace(xL,xR,IMAX+1);
 tend = 207000;          %set the final simulation time
 time = 0;               %initial time
 
 % initialize variables
 theta = zeros(1,IMAX);
-thetaNew = zeros(1,IMAX);
-psi = zeros(1,IMAX);
-T = zeros(1,IMAX);
-K = zeros(1,IMAX);
+volume = zeros(1,IMAX+1);
+volumeNew = zeros(1,IMAX+1);
+psi = zeros(1,IMAX+1);
+T = zeros(1,IMAX+1);
+a = zeros(1,IMAX+1);
+b = zeros(1,IMAX+1);
+c = zeros(1,IMAX+1);
+rhs = zeros(1,IMAX+1);
+K = zeros(1,IMAX+1);
 v = zeros(1,IMAX+1);
-rhs = zeros(1,IMAX);
-a = zeros(1,IMAX);
-b = zeros(1,IMAX);
-c = zeros(1,IMAX);
-f = zeros(1,IMAX);
-fk = zeros(1,IMAX);
+f  = zeros(1,IMAX+1);
+fk = zeros(1,IMAX+1);
 
 %set the initial condition
-for i=1:IMAX
-    psi(i) = -x(i);      % hydrostatic pressure
+for i=1:IMAX+1
+    if(i==IMAX+1)
+        psi(i) = -2.0;%psi(IMAX)-dx/2;   % psi at surface: if >0 ponding, otherwise not
+    else
+        psi(i) = -x(i);      % hydrostatic pressure
+    end
     T(i) = 19;
 end
 
@@ -54,48 +63,31 @@ end
 NMAX=100000;
 for nit=1:NMAX
     disp(sprintf('time iteration:%d', nit ));
+    dt = 300;
     % Right boundary conditions
-    psiR=1.0;
+    Rain=0/1000/300;
     TR=220;
     % Left boundary conditions
     psiL =0.0;
     TL=19;
-    
-    KR = kappa(psiR,TR);
+ 
     KL = kappa(psiL,TL);
-    for i=1:IMAX
-        theta(i)=Thetaf(psi(i));
-        K(i) = kappa(psi(i),T(i));
+    for i=1:IMAX+1
+        if(i==IMAX+1)
+            volume(i) = Hf(psi(i));
+            K(i) = kappa(psi(i)); % hydaraulic conductivity at soil surface
+        else
+            volume(i) = Thetaf(psi(i))*dx;
+            theta(i) = Thetaf(psi(i));
+            K(i) = kappa(psi(i));
+        end
     end
     
     if(time>=tend)
         break
     end
     
-    % compute velocities at time level n to determine the maximum time
-    % step (this is an approximation)
-    for i=1:IMAX+1
-        if i==1
-            k=0.5*(KL+K(i));
-            v(i) = -k*(psi(i)-psiL)/(dx/2)-k;
-        elseif i==IMAX+1
-            k=0.5*(KR+K(i-1));
-            v(i) = -k*(psiR-psi(i-1))/(dx/2)-k;
-        else
-            k=0.5*(K(i)+K(i-1));
-            v(i) = -k*(psi(i)-psi(i-1))/dx-k;
-        end
-    end
-    %
-    vmax=max(abs(v));
-    if vmax==0
-           dt=1000; % no advection therefore no stability requirement on dt
-    else
-        dt = min(1000, 0.9*CT(thetas)/( 1/dx*0.5*rho*cW*(2*vmax) ));
-    end
-    dt = 300;
-    % store all dt
-    %dtn(nit)=dt;
+   
     if(time+dt>tend)
         dt=tend-time;
     end
@@ -106,7 +98,7 @@ for nit=1:NMAX
     ylabel('Psi [m]')
     title(sprintf('Current time = %f',time))
     subplot(3,1,2)
-    plot(x,theta,'o')
+    plot(x,volume,'o')
     ylabel('Theta [-]')
 %     subplot(4,1,3)
 %     plot(xv,v,'o')
@@ -116,85 +108,108 @@ for nit=1:NMAX
     xlabel('z [m]')
     drawnow
     
-    % compute right hand side and the linear part of the system
-    for i=1:IMAX
+    %compute right hand side and the linear part of the system
+    for i=1:IMAX+1
         if(i==1)
             Kp = 0.5*(K(i)+K(i+1));
             Km = 0.5*(K(i)+KL);
-            % dirichlet (pressure) boundary condition
-            rhs(i) = theta(i) + dt/dx*(Kp-Km) + 2*Km*dt/dx^2*psiL;
-            a(i) = 0;
-            b(i) = dt/dx^2*(2*Km+Kp);
-            c(i) = -Kp*dt/dx^2;
-            
-            % no flux boundary
-            % rhs(i) = theta(i) + dt/dx*(Kp);
-        elseif(i==IMAX)
-            Kp = 0.5*(K(i)+KR);
+            %Kp = max(K(i),K(i+1));
+            %Km = max(K(i),KL);
+            a(i) = 0;                   % lower diagonal -> moved to the rhs multiplied by (-2)*psiL
+            b(i) = +(2*Km+Kp)*dt/dx*thetaMethod;  % diagonal(not rhs!)
+            c(i) = -Kp*dt/dx*thetaMethod;         % upper diagonal
+            rhs(i) = volume(i) + dt*(Kp-Km) + dt*Kp*(1-thetaMethod)*(psi(i+1)-psi(i))/dx...
+                    - 2*Km*dt/dx*(1-thetaMethod)*psi(i) + 2*Km*dt/dx*psiL;  % right hand side <- the "b" of lecture notes
+        elseif(i==IMAX+1)
+            Kp = 0;
             Km = 0.5*(K(i)+K(i-1));
-            % dirichlet (pressure boundary condition
-            rhs(i) = theta(i) + dt/dx*(Kp-Km) + 2*Kp*dt/dx^2*psiR;
-            a(i) = -Km*dt/dx^2;
-            b(i) = dt/dx^2*(Km+2*Kp);
+            %Kp = 0;
+            %Km = max(K(i),K(i-1));
+            %rainfall rate (Rain) boundary condition
+            a(i) = - dt/dx*Km*2*thetaMethod;
+            b(i) = + dt/dx*(2*Km+0)*thetaMethod;
             c(i) = 0;
-           
-            % no flux boundary
-            % rhs(i) = theta(i) + dt/dx*(-Km);
+            rhs(i) = volume(i) + dt*(Rain-Km) - 2*dt*Km*(1-thetaMethod)*(psi(i)-psi(i-1))/dx;
+        % questo if serve perche` a differenza del codice java in cui ho le
+        % distanze dei centroidi qui ho solo un dx costante
+        elseif(i==IMAX)
+            Kp = 0.5*(K(i)+K(i+1));
+            Km = 0.5*(K(i)+K(i-1));
+            a(i) = - dt/dx*Km*thetaMethod;
+            b(i) = + dt/dx*(Km+2*Kp)*thetaMethod;
+            c(i) = - 2*dt/dx*Kp*thetaMethod;
+            rhs(i) = volume(i) + dt*(Kp-Km) + 2*dt*Kp*(1-thetaMethod)*(psi(i+1)-psi(i))/dx...
+                    - dt*Km*(1-thetaMethod)*(psi(i)-psi(i-1))/dx;    
         else
             Kp = 0.5*(K(i)+K(i+1));
             Km = 0.5*(K(i)+K(i-1));
-            rhs(i) = theta(i) + dt/dx*(Kp-Km);
-            a(i) = -Km*dt/dx^2;
-            b(i) = dt/dx^2*(Km+Kp);
-            c(i) = -Kp*dt/dx^2;
+            a(i) = -Km*dt/dx*thetaMethod;         % lower diagonal
+            b(i) = +(Km+Kp)*dt/dx*thetaMethod;    % diagonal(not rhs!)
+            c(i) = -Kp*dt/dx*thetaMethod;         % upper diagonal
+            rhs(i) = volume(i) + dt*(Kp-Km) + dt*Kp*(1-thetaMethod)*(psi(i+1)-psi(i))/dx...
+                    - dt*Km*(1-thetaMethod)*(psi(i)-psi(i-1))/dx;  % right hand side <- the "b" of lecture notes
         end
     end
     
-    tol = 5e-15; %now we start with the nested Newton method
     
-    psi = min(psi,psic); % initial guess
+    %% Newton method
+    tol = 5e-14; %now we start with the nested Newton method
+    
+    % initial guess
+    psi(IMAX+1) = max(psi(IMAX+1),0.1);
+    psi(1:IMAX) = min(psi(1:IMAX),psic);%psic1
+    
     for iNewton=1:100 %outer Newton iterations
         
-        for i=1:IMAX
-            %compute the true non linear function f(psi)
-            f(i) = Thetaf(psi(i))-rhs(i);
-            if(i==1)
-                f(i) = f(i) + b(i)*psi(i) + c(i)*psi(i+1);
-            elseif(i==IMAX)
-                f(i) = f(i)+a(i)*psi(i-1)+b(i)*psi(i);
+        di = zeros(1,IMAX+1);
+       
+        for i=1:IMAX+1
+            if(i==IMAX+1)
+                f(i) = Hf(psi(i)) + a(i)*psi(i-1) + b(i)*psi(i) - rhs(i);
+            elseif(i==1)
+                f(i) = Thetaf(psi(i))*dx + a(i)*psiL + b(i)*psi(i) + c(i)*psi(i+1) - rhs(i);
             else
-                f(i) = f(i)+a(i)*psi(i-1)+b(i)*psi(i)+c(i)*psi(i+1);
+                f(i) = Thetaf(psi(i))*dx + a(i)*psi(i-1) + b(i)*psi(i) + c(i)*psi(i+1) - rhs(i);
             end
         end
+
         outres = sqrt(sum(f.*f)); %outer residual
-        %disp(sprintf('Outer iteration %d, outres=%e',iNewton, outres));
+        disp(sprintf('    Outer iteration %d, outres=%e',iNewton, outres));
         if(outres<tol)
             break %tolerance has been reached
         end
         
-        psik = psi;             % save the value at the current outer iteration
-        psi = max(psi,psic); % initial guess for the inner iteration
-        for inner=1:100
+        psik = psi;  % save the value at the current outer iteration
+        psi(IMAX+1) = max(psi(IMAX+1),0.1);
+        psi(1:IMAX) = max(psi(1:IMAX),psic);%psic1
+        for inner=1:100 % inner Newton step
             
-            for i=1:IMAX
-                fk(i)=Theta1(psi(i)) - ( Theta2(psik(i)) + dTheta2(psik(i))*(psi(i)-psik(i)) ) - rhs(i);  %Tk is frozen
-                if(i==1)
-                    fk(i)=fk(i)+b(i)*psi(i)+c(i)*psi(i+1);
-                elseif(i==IMAX)
-                    fk(i)=fk(i)+a(i)*psi(i-1)+b(i)*psi(i);
+            di = zeros(1,IMAX+1);
+            
+            for i=1:IMAX+1
+                if(i==IMAX+1)
+                    fk(i) = H1(psi(i)) + a(i)*psi(i-1) + b(i)*psi(i) - rhs(i);
+                    di(i) = dH1(psi(i));
+                elseif(i==1)
+                    fk(i) = Theta1(psi(i))*dx - ( Theta2(psik(i))*dx + dTheta2(psik(i))*dx*(psi(i)-psik(i)) ) + a(i)*psiL + b(i)*psi(i) + c(i)*psi(i+1) - rhs(i);
+                    di(i) = dTheta1(psi(i))*dx - dTheta2(psik(i))*dx;
                 else
-                    fk(i)=fk(i)+a(i)*psi(i-1)+b(i)*psi(i)+c(i)*psi(i+1);
+                    fk(i) = Theta1(psi(i))*dx - ( Theta2(psik(i))*dx + dTheta2(psik(i))*dx*(psi(i)-psik(i)) ) + a(i)*psi(i-1) + b(i)*psi(i) + c(i)*psi(i+1) - rhs(i);
+                    di(i) = dTheta1(psi(i))*dx - dTheta2(psik(i))*dx;
                 end
-                di(i)=dTheta1(psi(i))-dTheta2(psik(i));
             end
+
             inres=sqrt(sum(fk.*fk));
-            %disp(sprintf(' -Inner iteration %d, inres= %e', inner,inres));
+            disp(sprintf('        Inner iteration %d, inres= %e', inner,inres));
             if(inres<tol)
                 break
             end
-            dpsi = Thomas(a,b+di,c,fk); %inner Newton step
-            psi = psi(:)-dpsi(:); %update temperature at the inner iteration
+
+            dpsi = Thomas(a,b+di,c,fk);
+            psi(:) = psi(:) - dpsi(:);  %update psi at the inner iteration
+            
         end
+        
         
     end
     
@@ -209,8 +224,8 @@ for nit=1:NMAX
             % no flux boundary
             % v(i) = 0;
         elseif i==IMAX+1
-            k=0.5*(KR+K(i-1));
-            v(i) = -k*(psiR-psi(i-1))/(dx/2)-k;
+            k=0.5*(K(i)+K(i-1));
+            v(i) = -k*(psi(i)-psi(i-1))/(dx/2)-k;
             % no flux boundary
             % v(i) = 0;
         else
@@ -220,16 +235,16 @@ for nit=1:NMAX
         end
     end
     
-    errorMass(nit)=(sum(thetaNew) - sum(theta) + dt/dx*(v(IMAX+1)-v(1)));
-    disp(sprintf('    Error mass:%e', errorMass(nit) ));
+    errorMass(nit)=(sum(thetaNew) - sum(theta) - dt/dx*(Rain- 0.5*( KL+K(1) )*( (psi(1)-psiL)/(dx/2) +1 ) ) );
+    disp(sprintf('    Error mass:%e', errorMass(nit) ) );
     
-    [T, errorHeat(nit)]= ConvectionDiffusionImplicit(T,v,theta,thetaNew);
+    %[T, errorHeat(nit)]= ConvectionDiffusionImplicit(T,v,theta,thetaNew);
     %[T, errorHeat(nit)]= Diffusion(T,theta);
-    disp(sprintf('    Error heat:%e', errorHeat(nit) ));
-    disp(sprintf('    Tmax:%f', max(T) ));
-    if(max(T)>TR)
-        break
-    end
+    %disp(sprintf('    Error heat:%e', errorHeat(nit) ));
+    %disp(sprintf('    Tmax:%f', max(T) ));
+    %if(max(T)>TR)
+    %    break
+    %end
     time = time+dt;     %advance time
 end
 
