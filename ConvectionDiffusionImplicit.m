df7a552ea49e0ca0d62084387600d128e9395e86
niccolotubini <tubini.niccolo@gmail.com>
2018-10-11 10:32:02 +0200

    Adde the surface layer. It works only if there is no ponding and no rain or if rains and there is always ponding water

--- ConvectionDiffusionImplicit.m
+++ ConvectionDiffusionImplicit.m
@@ -2,11 +2,11 @@
 % convection explicit in time, Godunov flux (upwind)
 % diffusion implicit in time (time restriction due to diffusion is too severe)
 
-function [Tnew, error]=ConvectionDiffusionImplicit(T,u,theta,thetaNew)
-global IMAX dx dt rho cW TR TL
+function [Tnew, error]=ConvectionDiffusionImplicit(T,u,theta,thetaNew,volume,volumeNew)
+global IMAX dx dt rho cW TR TL Rain
 % Total internal energy at time level n
 E=0;
-for i=1:IMAX
+for i=1:IMAX+1
     if(i==1)       
         lambdap = 0.5*(Lambda(theta(i))+Lambda(theta(i+1)));
         lambdam = 0.5*(Lambda(theta(i))+Lambda(theta(i)));
@@ -14,13 +14,27 @@ for i=1:IMAX
         %fp =   rho*cW*( 0.5*u(i+1)*(T(i+1)+T(i)) - 0.5*abs(u(i+1))*(T(i+1)-T(i)) );
         %fm =   rho*cW*( 0.5*u(i  )*(T(i  )+TL  ) - 0.5*abs(u(i  ))*(T(i  )-TL  ) );
 
-        rhs(i) = CT(theta(i))*T(i) - dt/dx*rho*cW*( -0.5*u(i  )*TL - 0.5*abs(u(i  ))*TL ) + 2*dt/dx^2*lambdam*TL;
+        rhs(i) = CT(theta(i))*T(i)*dx - dt*rho*cW*( -0.5*u(i  )*TL - 0.5*abs(u(i  ))*TL ) + 2*dt/dx*lambdam*TL;
         a(i) = 0;
-        b(i) = CT(thetaNew(i)) + dt/dx^2*(lambdap+2*lambdam) + dt/dx*rho*cW*(  0.5*u(i+1) + 0.5*abs(u(i+1)) - ( 0.5*u(i) - 0.5*abs(u(i)) ) );
-        c(i) = -dt/dx^2*lambdap + dt/dx*rho*cW*( 0.5*u(i+1) - 0.5*abs(u(i+1)) );
+        b(i) = CT(thetaNew(i))*dx + dt/dx*(lambdap+2*lambdam) + dt*rho*cW*(  0.5*u(i+1) + 0.5*abs(u(i+1)) - ( 0.5*u(i) - 0.5*abs(u(i)) ) );
+        c(i) = -dt/dx*lambdap + dt*rho*cW*( 0.5*u(i+1) - 0.5*abs(u(i+1)) );
         
         heatFluxBottom = 2/dx*lambdam*TL + rho*cW*(0.5*u(i  )*TL + 0.5*abs(u(i  ))*TL );
-        E = E + CT(theta(i))*T(i);
+        E(i) = + CT(theta(i))*T(i)*dx;
+    elseif(i==IMAX+1)
+        %lambdap = 0.5*(Lambda(theta(i-1))+Lambda(theta(i-1)));
+        lambdam = 0.5*(Lambda(theta(i-1))+Lambda(theta(i-1)));
+        % convection flux fp at interface i+1/2 and fm at i-1/2
+        %fp =  rho*cW*( 0.5*u(i+1)*(TR  +T(i  )) - 0.5*abs(u(i+1))*(TR  -T(i  )) );
+        %fm =  rho*cW*( 0.5*u(i  )*(T(i)+T(i-1)) - 0.5*abs(u(i  ))*(T(i)-T(i-1)) );
+
+        rhs(i) = volume(i)*rho*cW*T(i) + dt*0.00 + dt*rho*cW*( Rain*TR );
+        a(i) = -2*dt/dx*lambdam + dt*rho*cW*(  -0.5*u(i  ) - 0.5*abs(u(i  )) );
+        b(i) = volumeNew(i)*rho*cW + 2*dt/dx*(lambdam) + dt*rho*cW*( - 0.5*u(i) + 0.5*abs(u(i) ) );
+        c(i) = 0;
+        
+        heatFluxTop = 0 + rho*cW*Rain*TR;
+        E(i) = + volumeNew(i)*rho*cW*T(i);
     elseif(i==IMAX)
         lambdap = 0.5*(Lambda(theta(i))+Lambda(theta(i)));
         lambdam = 0.5*(Lambda(theta(i-1))+Lambda(theta(i)));
@@ -28,13 +42,13 @@ for i=1:IMAX
         %fp =  rho*cW*( 0.5*u(i+1)*(TR  +T(i  )) - 0.5*abs(u(i+1))*(TR  -T(i  )) );
         %fm =  rho*cW*( 0.5*u(i  )*(T(i)+T(i-1)) - 0.5*abs(u(i  ))*(T(i)-T(i-1)) );
 
-        rhs(i) = CT(theta(i))*T(i) + 2*dt/dx^2*lambdap*TR - dt/dx*rho*cW*( 0.5*u(i+1)*TR  - 0.5*abs(u(i+1))*TR );
-        a(i) = -dt/dx^2*lambdam + dt/dx*rho*cW*(  -0.5*u(i  ) - 0.5*abs(u(i  )) );
-        b(i) = CT(thetaNew(i)) + dt/dx^2*(2*lambdap+lambdam) + dt/dx*rho*cW*(  0.5*u(i+1) + 0.5*abs(u(i+1)) - ( 0.5*u(i) - 0.5*abs(u(i)) ) );
-        c(i) = 0;
+        rhs(i) = CT(theta(i))*T(i)*dx;
+        a(i) = -dt/dx*lambdam + dt*rho*cW*(  -0.5*u(i  ) - 0.5*abs(u(i  )) );
+        b(i) = CT(thetaNew(i))*dx + dt/dx*(2*lambdap+lambdam) + dt*rho*cW*(  0.5*u(i+1) + 0.5*abs(u(i+1)) - 0.5*u(i) + 0.5*abs(u(i) ) );
+        c(i) = -2*dt/dx*lambdap + dt*rho*cW*( 0.5*u(i+1) - 0.5*abs(u(i+1)) );
         
-        heatFluxTop = - 2/dx*lambdap*TR + rho*cW*( 0.5*u(i+1)*TR  - 0.5*abs(u(i+1))*TR );
-        E = E + CT(theta(i))*T(i);
+        %heatFluxTop = - 2/dx*lambdap*TR + rho*cW*( 0.5*u(i+1)*TR  - 0.5*abs(u(i+1))*TR );
+        E(i) = + CT(theta(i))*T(i)*dx;
     else
         lambdap = 0.5*(Lambda(theta(i))+Lambda(theta(i+1)));
         lambdam = 0.5*(Lambda(theta(i-1))+Lambda(theta(i)));
@@ -42,12 +56,12 @@ for i=1:IMAX
 %         fp = rho*cW*( 0.5*u(i+1)*(T(i+1)+T(i  )) - 0.5*abs(u(i+1))*(T(i+1)-T(i  )) );
 %         fm = rho*cW*( 0.5*u(i  )*(T(i  )+T(i-1)) - 0.5*abs(u(i  ))*(T(i  )-T(i-1)) );
 
-        rhs(i) = CT(theta(i))*T(i);
-        a(i) = -dt/dx^2*lambdam + dt/dx*rho*cW*( -0.5*u(i) - 0.5*abs(u(i)) );
-        b(i) = CT(thetaNew(i)) + dt/dx^2*(lambdap+lambdam)  + dt/dx*rho*cW*(  0.5*u(i+1) + 0.5*abs(u(i+1)) - ( 0.5*u(i) - 0.5*abs(u(i)) ) );
-        c(i) = -dt/dx^2*lambdap + dt/dx*rho*cW*( 0.5*u(i+1) - 0.5*abs(u(i+1)) );
+        rhs(i) = CT(theta(i))*T(i)*dx;
+        a(i) = -dt/dx*lambdam + dt*rho*cW*( -0.5*u(i) - 0.5*abs(u(i)) );
+        b(i) = CT(thetaNew(i))*dx + dt/dx*(lambdap+lambdam)  + dt*rho*cW*(  0.5*u(i+1) + 0.5*abs(u(i+1)) - 0.5*u(i) + 0.5*abs(u(i) ) );
+        c(i) = -dt/dx*lambdap + dt*rho*cW*( 0.5*u(i+1) - 0.5*abs(u(i+1)) );
         
-        E = E + CT(theta(i))*T(i);
+        E(i) = + CT(theta(i))*T(i)*dx;
     end
     
 end
@@ -56,15 +70,16 @@ Tnew=Thomas(a,b,c,rhs);
 % compute the total internal energy at time level n+1
 Enew=0;
 for i=1:IMAX
-    Enew = Enew + CT(thetaNew(i))*Tnew(i);
+    Enew(i) = + CT(thetaNew(i))*Tnew(i)*dx;
 end
-heatFluxTop    = heatFluxTop    + 2*(0.5*( Lambda(theta(IMAX)) + Lambda(theta(IMAX)) ) ) *Tnew(IMAX)/dx;
+Enew(IMAX+1) =+volumeNew(IMAX+1)*rho*cW*Tnew(IMAX+1);
+heatFluxTop    = heatFluxTop;%    + 2*(0.5*( Lambda(theta(IMAX)) + Lambda(theta(IMAX)) ) ) *Tnew(IMAX)/dx;
 heatFluxBottom = heatFluxBottom - 2*(0.5*(Lambda(theta(1))+Lambda(theta(1))))*Tnew(1)/dx;
 
 % Absolute error of internal energy E^n+1 = E^n - dt/dx*(fluxTop - fluxBottom)
 %error = Enew - E + dt/dx*(heatFluxTop-heatFluxBottom);
-
-error = Enew - E + dt/dx*( rho*cW*0.5*( u(IMAX+1)*(TR+Tnew(IMAX)) - abs(u(IMAX+1))*(TR-Tnew(IMAX)) ) -Lambda(theta(IMAX))*2*(TR-Tnew(IMAX))/dx ...
+Enew-E;
+error = sum(Enew) - sum(E) + dt*( rho*cW*( Rain*TR )  ...
                             -rho*cW*0.5*( u(1)*(Tnew(1)+TL) - abs(u(1))*(Tnew(1)-TL) ) + Lambda(theta(1))*2*(Tnew(1)-TL)/dx );
 error = error;
 end
